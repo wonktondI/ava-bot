43990d8b1bbc75f7c80f1b162e460f2c9f483549
naooohu <naooohu@gmail.com>
2024-01-18 03:02:45 +0800

    finish!🎉

--- src/handlers/chats.rs
+++ src/handlers/chats.rs
@@ -1,17 +1,53 @@
-use std::convert::Infallible;
-use futures_util::StreamExt;
+use crate::handlers::{AssistantEvent, COOKIE_NAME};
+use crate::EVENTS;
+use dashmap::DashMap;
+use salvo::prelude::SseKeepAlive;
 use salvo::sse::SseEvent;
-use salvo::{handler, sse, Response};
+use salvo::{handler, Request, Response};
+use std::convert::Infallible;
 use std::time::Duration;
-use tokio::time::interval;
-use tokio_stream::wrappers::IntervalStream;
+use tokio::sync::broadcast;
+use tokio_stream::{wrappers::BroadcastStream, StreamExt as _};
+use tracing::info;
 
+const MAX_EVENTS: usize = 128;
 #[handler]
-pub async fn chats_handler(res: &mut Response) {
-    let event_stream = {
-        let interval = interval(Duration::from_secs(1));
-        let stream = IntervalStream::new(interval);
-        stream.map(move |_| Ok::<SseEvent, Infallible>(SseEvent::default().text("<li>hello world!</li>".to_string())))
+pub async fn events_handler(req: &mut Request, res: &mut Response) {
+    let device_id = req.cookie(COOKIE_NAME).unwrap().value().to_owned();
+
+    info!("user {device_id} connected");
+    sse_handler(&device_id, &EVENTS, res).await;
+}
+
+async fn sse_handler(
+    device_id: &str,
+    map: &DashMap<String, broadcast::Sender<AssistantEvent>>,
+    res: &mut Response,
+) {
+    let rx = if let Some(tx) = map.get(device_id) {
+        tx.subscribe()
+    } else {
+        let (tx, rx) = broadcast::channel(MAX_EVENTS);
+        map.insert(device_id.to_string(), tx);
+        rx
     };
-    sse::stream(res, event_stream);
+
+    let stream = BroadcastStream::new(rx)
+        .filter_map(|v| v.ok())
+        .map(|v| {
+            let (event, id) = match &v {
+                AssistantEvent::Signal(_) => ("signal", "".to_string()),
+                AssistantEvent::InputSkeleton(_) => ("input_skeleton", "".to_string()),
+                AssistantEvent::Input(v) => ("input", v.id.clone()),
+                AssistantEvent::ReplySkeleton(_) => ("reply_skeleton", "".to_string()),
+                AssistantEvent::Reply(v) => ("reply", v.id.clone()),
+            };
+            let data: String = v.into();
+            SseEvent::default().text(data).name(event).id(id)
+        })
+        .map(Ok::<_, Infallible>);
+    SseKeepAlive::new(stream)
+        .comment("keep-alive-text")
+        .max_interval(Duration::from_secs(1))
+        .stream(res);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,42 @@
 use anyhow::Result;
-use ava_bot::handlers::{chats_handler, index_page};
+use ava_bot::handlers::{assistant_handler, events_handler, index_page};
+use ava_bot::Args;
 use clap::Parser;
+use rust_embed::RustEmbed;
 use salvo::prelude::TcpListener;
-use salvo::serve_static::StaticDir;
+use salvo::serve_static::{static_embed, StaticDir};
 use salvo::{Listener, Router, Server};
 use std::time::Duration;
 use tracing::info;
 
-#[derive(Debug, Parser)]
-#[clap(name = "ava")]
-struct Args {
-    #[clap(short, long, default_value = "8080")]
-    port: u16,
-    #[clap(short, long, default_value = ".certs")]
-    cert_path: String,
-}
+#[derive(RustEmbed)]
+#[folder = "./public/"]
+struct Public;
+
+#[derive(RustEmbed)]
+#[folder = "./tmp/ava-bot/"]
+struct Asset;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_line_number(true)
+        .with_max_level(tracing::Level::INFO)
+        .init();
 
     let args = Args::parse();
     let router = Router::new()
-        .push(Router::with_path("/public/<*path>").get(StaticDir::new(["public"]).auto_list(true)))
+        // .push(Router::with_path("/public/<*path>").get(StaticDir::new(["public"]).auto_list(true)))
+        .push(Router::with_path("/public/<*path>").get(static_embed::<Public>()))
         .push(
-            Router::with_path("/")
+            // Router::with_path("/assets/<*path>").get(StaticDir::new(["tmp/ava-bot"]).auto_list(true)),
+            Router::with_path("/assets/<*path>").get(static_embed::<Asset>()),
+        )
+        .push(
+            Router::new()
                 .get(index_page)
-                .push(Router::with_path("/chats").get(chats_handler)),
+                .push(Router::with_path("/events").get(events_handler))
+                .push(Router::with_path("/assistant").post(assistant_handler)),
         );
 
     let addr = format!("0.0.0.0:{}", args.port);
